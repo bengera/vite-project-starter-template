af6856e883990f095600ae22dd5519e1b0f05a8a
Ben <bengeraghty.bg@gmail.com>
2024-02-10 19:11:55 +0700

    add postcss config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,7 +3,12 @@ const postcssPresetEnv = require("postcss-preset-env");
 module.exports = {
   plugins: [
     postcssPresetEnv({
-      stage: 0,
+      stage: 3,
+      features: {
+        "logical-properties-and-values": false,
+      },
     }),
   ],
 };
+
+// use stage 3 but don't touch logical properties - values
