8acd9621c5886b35a26d1ec632dd9d4ffecfc6b6
Ben <61493854+bengera@users.noreply.github.com>
2024-11-01 15:59:37 +0700

    update files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
-import htmlPurge from 'vite-plugin-purgecss'
+import { defineConfig } from 'vite';
+import htmlPurge from 'vite-plugin-purgecss';
+import { resolve } from 'path';
 
-export default {
-    plugins: [
-        htmlPurge(),
-    ]
-}
+export default defineConfig({
+  base: '/',
+  css: {
+    preprocessorOptions:{
+      scss: {
+      api: 'modern-compiler'
+      }
+    }
+  },
+  plugins: [
+    htmlPurge(),
+  ],
+  build: {
+    assetsInlineLimit: 0, // Disable inline asset embedding for all asset types
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        mainJS: resolve(__dirname, 'main.js')
+        
+      }
+    }
+  }
+});
+
